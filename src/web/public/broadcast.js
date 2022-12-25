e1ca4d04ff701003213cc0869757c3cb3f4285cb
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-12-25 13:52:46 +0800

    [change]可以直播啦

--- src/web/public/broadcast.js
+++ src/web/public/broadcast.js
@@ -1,34 +1,168 @@
-//需和 queue.js 一起引用
+// 配置
+const chunkSize = 4096; // 单个直播包大小
+const sendInterval = 1000; // 直播包发送间隔
+const recordInterval = 100; // 直播fps
+// ========
 
-class Broadcast {
-    constructor() {
-        this.available = false; // 变量：是否可用
-        this.stream = null; // 内部对象：数据流
-    }
+const socket = io();
+var params = {}
+window.location.search.slice(1).split('&').forEach(v => {
+    var _ = v.split('=');
+    params[_[0]] = _[1];
+})
+if (!params.type || !params.bid || !params.name) window.location.pathname = '/';
 
-    async start() {
-        if (this.available) return;
-        try {
-            this.stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-        } catch (err) {
-            console.warn("User canceled the recorder. More info:", err)
-            return false;
+if (params.type == 'create') {
+    document.querySelector('#show').style = 'display: none;';
+} else {
+    document.querySelector('#broadcast').style = 'display: none;';
+}
+
+function getImg() {
+    var player = document.getElementById('broadcast');
+    var canvas = document.createElement('canvas');
+    canvas.width = document.querySelector('#broadcast').videoWidth;
+    canvas.height = document.querySelector('#broadcast').videoHeight;
+    var ctx = canvas.getContext("2d");
+
+    ctx.drawImage(player, 0, 0)
+
+    return canvas.toDataURL(undefined, 0.5);
+}
+
+function start() {
+    navigator.mediaDevices.getDisplayMedia({
+        video: true
+    }).then(stream => {
+        var player = document.getElementById('broadcast')
+        if ("srcObject" in player) {
+            player.srcObject = stream;
+        } else {
+            player.src = window.URL.createObjectURL(stream);
         }
-        this.available = true;
-        return true;
-    }
+        player.onloadedmetadata = function(e) {
+            player.play();
+        }
+    }).catch(err => {
+        socket.emit('chat', `There's an error ocurrated in host. Error content:${err}. Can't broadcast!`)
+    })
 
-    pause() {
-        if (!this.available) return;
-        this._stream = this.stream.clone();
-        this.stream = null;
-        this.available = false;
-    }
+    var frames = [];
+    setInterval(() => {
+        var img = getImg();
+        frames.push(img);
+    }, recordInterval);
+    setInterval(() => {
+        for (let frameId = 0; frameId < frames.length; frameId++) {
+            const img = frames[frameId];
+            var chunks = [];
 
-    reseum() {
-        if (this.available) return;
-        this.stream = this._stream.clone();
-        this._stream = null;
-        this.available = true;
+            var i = 0;
+
+            while (i <= img.length - chunkSize) {
+                chunks.push(img.slice(i, i + chunkSize));
+                i += chunkSize;
+            }
+
+            for (let chunkId = 0; chunkId < chunks.length; chunkId++) {
+                const chunk = chunks[chunkId];
+                socket.emit('data', {
+                    chunk,
+                    chunkId,
+                    frameId,
+                    timestamp: (new Date()).valueOf(),
+                    width: document.querySelector('#broadcast').videoWidth,
+                    height: document.querySelector('#broadcast').videoHeight
+                })
+            }
+
+        }
+        frames = [];
+    }, sendInterval);
+}
+
+socket.on("auth", () => {
+    socket.emit("auth", {
+        type: params.type,
+        bid: params.bid,
+        name: params.name
+    });
+})
+
+socket.on("err_auth", () => {
+    alert("Auth failed, Please rejoin!");
+    window.location.pathname = '/'
+});
+socket.on("room_using", () => {
+    alert("Room is in using, Please change broadcast id!");
+    window.location.pathname = '/create'
+})
+socket.on("room_not_found", () => {
+    alert("Room not found, Please check broadcast id!");
+    window.location.pathname = '/join'
+})
+socket.on("authed", () => {
+    alert("Successfully login, enjoy :)");
+    if (params.type == 'create') {
+        start();
     }
+})
+socket.on('user_join', (name) => {
+    var dom = document.createElement("li");
+    dom.innerText = name + ' join the room.';
+    document.querySelector('#msg').appendChild(dom);
+})
+socket.on("user_leave", (data) => {
+    var dom = document.createElement("li");
+    dom.innerText = data.name + ' left the room. Reason: ' + data.reason;
+    document.querySelector('#msg').appendChild(dom);
+})
+socket.on('message', (data) => {
+    var dom = document.createElement("li");
+    dom.innerText = data.name + ' : ' + data.msg;
+    document.querySelector('#msg').appendChild(dom);
+})
+
+if (params.type == 'join') {
+    var media = document.querySelector('#show');
+    var frameQueue = [];
+    var currentFrame = 0;
+
+    socket.on('data', ({
+        chunk,
+        chunkId,
+        frameId,
+        timestamp,
+        width,
+        height
+    }) => {
+        if (chunkId == 0) {
+            frameQueue[frameId] = chunk;
+        } else {
+            frameQueue[frameId] += chunk;
+        }
+    })
+    setInterval(() => {
+        currentFrame++;
+        media.src = frameQueue[currentFrame] || '';
+        if (currentFrame > Math.floor(sendInterval / recordInterval)) {
+            currentFrame = 0;
+        }
+    }, recordInterval);
+}
+
+window.addEventListener("keypress", ({
+    code
+}) => {
+    if (code == 'Enter') sendMsg();
+})
+
+function sendMsg() {
+    var msg = document.querySelector('#chat');
+    var v = msg.value;
+    socket.emit('chat', v);
+    var dom = document.createElement("li");
+    dom.innerText = params.name + ' : ' + v;
+    document.querySelector('#msg').appendChild(dom);
+    msg.value = '';
 }
